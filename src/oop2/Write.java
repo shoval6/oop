993737f0f6cb3b0a539c72463bf67d85e09a045b
shoval6 <33618381+shoval6@users.noreply.github.com>
2017-12-24 03:46:08 +0200

    Update for EX2

--- src/oop2/Write.java
+++ src/oop2/Write.java
@@ -13,12 +13,18 @@ public class Write {
 		String line = "Time, ID, Lat, Lon, Alt, #WiFi networks (up to 10), SSID1, MAC1, Frequncy1, Signal1, SSID2, MAC2, Frequncy2, Signal2, SSID3, MAC3, Frequncy3, Signal3, SSID4, MAC4, Frequncy4, Signal4, SSID5, MAC5, Frequncy5, Signal5, SSID6, MAC6, Frequncy6, Signal6, SSID7, MAC7, Frequncy7, Signal7, SSID8, MAC8, Frequncy8, Signal8, SSID9, MAC9, Frequncy9, Signal9, SSID10, MAC10, Frequncy10, Signal10";		
 		String path = name+".csv";
 		StringBuilder writer = new StringBuilder();
+		String flag1 = "FinalCSV" , flag2 = "FilteredCSV" , flag3 = "Algo1" , flag4 = "Algo2";
 		PrintWriter file = null;
 		try {
 			file = new PrintWriter(new File(path));
+			if(!(flag3.equals(name)))
 			writer.append(line + "\n");		
 			for(int i=0; i<CSV.size(); i++){
+				if(flag1.equals(name)||flag2.equals(name)||flag4.equals(name))
 				writer.append(CSV.get(i) + "\n");		
+				else if(flag3.equals(name))
+					writer.append(CSV.get(i).toStringAlgo1() + "\n");		
+
 			}
 			file.write(writer.toString());
 			file.close();
