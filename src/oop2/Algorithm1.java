5f33f7b45937309b40784a3c0240619cc56476fc
shoval6 <33618381+shoval6@users.noreply.github.com>
2017-12-24 03:46:58 +0200

    New Class Algo1

---
+++ src/oop2/Algorithm1.java
@@ -0,0 +1,40 @@
+package oop2;
+
+import java.util.ArrayList;
+
+public class Algorithm1 {
+
+	public ArrayList<WiFi> MacLocation(ArrayList<WiFi> Organized , int num){
+		
+		Calculating calc = new Calculating();
+		
+		
+		ArrayList<WiFi> Final = new ArrayList<WiFi>();
+		WiFi wifi = null;
+		WiFiSub sub = null;
+		String mac;
+		
+		for(int i=0; i<Organized.size(); i++){
+
+			for(int j=0; j<Organized.get(i).getWiFiSub().size(); j++){
+				sub = Organized.get(i).getWiFiSub().get(j);
+				mac = sub.getMAC();
+				wifi = calc.MacCompare(Organized, num, mac);
+				Final.add(wifi);
+				j--;
+			}
+		
+		}
+		
+		
+		return Final;
+		
+		
+		
+		
+	}
+	
+	
+
+	
+}
