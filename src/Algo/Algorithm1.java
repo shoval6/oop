bd3799698fead8e2dc4fcd4c2000c78207f21dae
shoval6 <shovalhm@gmail.com>
2018-01-04 20:51:03 +0200

    Add Package

---
+++ src/Algo/Algorithm1.java
@@ -0,0 +1,43 @@
+package Algo;
+
+import java.util.ArrayList;
+
+import WiFi.WiFi;
+import WiFi.WiFiSub;
+
+public class Algorithm1 {
+
+	public ArrayList<WiFi> MacLocation(ArrayList<WiFi> Organized , int num){
+		
+		Calculating calc = new Calculating();
+		
+		
+		ArrayList<WiFi> Final = new ArrayList<WiFi>();
+		WiFi wifi = null;
+		WiFiSub sub = null;
+		String mac;
+		
+		for(int i=0; i<Organized.size(); i++){
+
+			for(int j=0; j<Organized.get(i).getWiFiSub().size(); j++){
+				sub = Organized.get(i).getWiFiSub().get(j);
+				mac = sub.getMAC();
+				wifi = calc.MacCompare(Organized, num, mac);
+				Final.add(wifi);
+				j--;
+			}
+		
+		}
+		
+		
+		return Final;
+		
+		
+		
+		
+	}
+	
+	
+
+	
+}
