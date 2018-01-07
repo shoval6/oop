43265f8daaa8689cfded8a14d4444842367386d3
shoval6 <shovalhm@gmail.com>
2018-01-07 11:34:17 +0200

    Add class

---
+++ src/GUI/ReadSaveFilter.java
@@ -0,0 +1,70 @@
+package GUI;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import Filters.Filter;
+
+public class ReadSaveFilter {
+
+	public void SaveFilter(Filter fl) {
+
+
+
+		try {
+			FileOutputStream f = new FileOutputStream(new File("Filter.txt"));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+
+			// Write objects to file
+			o.writeObject(fl);
+
+			o.close();
+			f.close();
+
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		}
+
+	}
+
+	
+	public Filter ReadFilter(){
+		try{
+		Open op = new Open();
+		File file = op.PickFilterFile();
+		FileInputStream fi = new FileInputStream(new File(file.getPath()));
+		ObjectInputStream oi = new ObjectInputStream(fi);
+
+		// Read objects
+		Filter fl = (Filter) oi.readObject();
+
+		oi.close();
+		fi.close();
+		
+		return fl;
+		
+		}
+		
+		catch (IOException e) {
+			System.out.println("Error initializing stream");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+
+	}
+	
+}
+
+
+
+
